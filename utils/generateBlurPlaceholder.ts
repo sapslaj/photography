e80cdf4636fdb95f931953e521703213d998ea45
sapslaj <saps.laj@gmail.com>
2025-04-23 15:18:20 -0400

    add retries to getBase64ImageUrl
    I think Next is trying to be _too_ fast and causing fetch failures.

--- utils/generateBlurPlaceholder.ts
+++ utils/generateBlurPlaceholder.ts
@@ -1,23 +1,39 @@
-import imagemin from 'imagemin'
-import imageminJpegtran from 'imagemin-jpegtran'
-import type { ImageProps } from './types'
+import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
 
-const cache = new Map<ImageProps, string>()
+import type { ImageProps } from "./types";
+
+const cache = new Map<ImageProps, string>();
 
 export default async function getBase64ImageUrl(
   image: ImageProps
 ): Promise<string> {
-  let url = cache.get(image)
+  let url = cache.get(image);
   if (url) {
-    return url
+    return url;
   }
-  const response = await fetch(image.variants.blurplaceholder);
-  const buffer = await response.arrayBuffer()
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
-    plugins: [imageminJpegtran()],
-  })
+  for (let attempt = 1; attempt <= 5; attempt++) {
+    try {
+      const response = await fetch(image.variants.blurplaceholder);
+      const buffer = await response.arrayBuffer();
+      const minified = await imagemin.buffer(Buffer.from(buffer), {
+        plugins: [imageminJpegtran()],
+      });
 
-  url = `data:image/jpeg;base64,${Buffer.from(minified).toString('base64')}`
-  cache.set(image, url)
-  return url
+      url = `data:image/jpeg;base64,${Buffer.from(minified).toString(
+        "base64"
+      )}`;
+      cache.set(image, url);
+      return url;
+    } catch (e) {
+      console.warn(
+        `failed to generate base64 image URL for image ${image.filename}: ${e} (attempt ${attempt})`
+      );
+      await new Promise((resolve) => setTimeout(resolve, 1000 * attempt));
+      if (attempt == 5) {
+        console.info("image: ", JSON.stringify(image));
+        throw e;
+      }
+    }
+  }
 }
